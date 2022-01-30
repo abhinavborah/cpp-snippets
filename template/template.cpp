7e5580100d17023c7239eb19a5cb9a2ac5e6e339
Abhinav Borah <aborah080@gmail.com>
2022-01-30 15:49:11 +0530

    added template.cpp

---
+++ template/template.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int;
+#define ld long double;
+#define pb push_back;
+typedef priority_queue<int, vector<int>, greater<int>> minheap;
+typedef priority_queue<long long int> maxheap;
+#define IOS ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+
+void solve(){
+    
+}
+
+int main(){
+    IOS
+
+}
